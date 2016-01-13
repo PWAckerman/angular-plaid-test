af76ee4cbbcb54366bc97e9cbfe8c6386e3418b0
Jared Hensley <jahensle85@me.com>
2016-01-13 17:22:34 -0600

    controller files

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 'use strict';
 let plaid = require('plaid'),
-   express = require('express'),
-   plaid_env = plaid.environments.tartan,
-   plaid_config = require('./secrets'),
-   bodyParser = require('body-parser'),
-   environment = "development",
-   mongoo = require("./mongoose.js"),
-   plaidClient = {},
-   app = express(),
-   User = require("./models/user.model.js"),
-   Transaction = require("./models/transaction.model.js"),
-   userCtrl = require("./controllers/user.controller.js"),
-   secrets = require("./secrets.js"),
-   db = mongoo.db()
+  express = require('express'),
+  plaid_env = plaid.environments.tartan,
+  plaid_config = require('./secrets'),
+  bodyParser = require('body-parser'),
+  environment = "development",
+  mongoo = require("./mongoose.js"),
+  plaidClient = {},
+  app = express(),
+  User = require("./models/user.model.js"),
+  Transaction = require("./models/transaction.model.js"),
+  userCtrl = require("./controllers/user.controller.js"),
+  secrets = require("./secrets.js"),
+  db = mongoo.db()
 
-db.connection.once('open', ()=>{
+console.log(mongoo.db());
+console.log('db', db);
+console.log('mongoo', mongoo);
+
+db.connection.once('open', () => {
   console.log('Db is connected')
 })
 console.log(
-  plaid.environments
-)
-//
-// let bank= {
-//   type: 'amex'
-// }
-// let plaidTestApi = {
-//   id: 'test_id',
-//   secret: 'test_secret',
-//   access_token: 'test_' + bank.type
-// }
+    plaid.environments
+  )
+  //
+  // let bank= {
+  //   type: 'amex'
+  // }
+  // let plaidTestApi = {
+  //   id: 'test_id',
+  //   secret: 'test_secret',
+  //   access_token: 'test_' + bank.type
+  // }
 let plaidTestUser = {}
-if(environment === 'test'){
-   plaidClient = new plaid.Client("test_id", "test_secret", plaid_env);
+if (environment === 'test') {
+  plaidClient = new plaid.Client("test_id", "test_secret", plaid_env);
 }
-if(environment === 'development'){
+if (environment === 'development') {
   plaidClient = new plaid.Client(secrets.secrets.client_id, secrets.secrets.secret, plaid_env);
 }
 
@@ -78,9 +82,9 @@ if(environment === 'development'){
 app
   .use(bodyParser.json())
   .use(express.static(__dirname))
-  .post('/authenticate', function(req, response) {
+  .post('/authenticate', function (req, response) {
     var public_token = req.body.public_token;
-    plaidClient.exchangeToken(public_token, (err, res)=> {
+    plaidClient.exchangeToken(public_token, (err, res) => {
       if (err) {
         console.log(err)
       } else {
@@ -95,104 +99,111 @@ app
           (err, res) => {
             console.log(err)
             res.json(response)
-            // plaidClient.patchConnectUser(plaidTestUser.access_token, {}, {
-            //   webhook: 'http://www.pushbudget.com/api/incoming-webhook',
-            // }, function(err, mfaResponse, response) {
-            //   // The webhook URI should receive a code 4 "webhook acknowledged" webhook
-            //   console.log('HOOK ERR', err)
-            //   console.log('HOOK mfaResponse', mfaResponse)
-            //   console.log('HOOK response', response)
-            // });
+              // plaidClient.patchConnectUser(plaidTestUser.access_token, {}, {
+              //   webhook: 'http://www.pushbudget.com/api/incoming-webhook',
+              // }, function(err, mfaResponse, response) {
+              //   // The webhook URI should receive a code 4 "webhook acknowledged" webhook
+              //   console.log('HOOK ERR', err)
+              //   console.log('HOOK mfaResponse', mfaResponse)
+              //   console.log('HOOK response', response)
+              // });
           }
         )
-    }}
-  )})
-  .get('/users/all', (req, response)=>{
-    User.find((err, res)=>{
+      }
+    })
+  })
+  .get('/users/all', (req, response) => {
+    User.find((err, res) => {
       response.json(res)
     })
   })
-   .get('/plaidTransactions/:id', (req, response)=>{
-     console.log("Hit the endpoint...")
-     User.findById(req.params.id).exec((err, res)=>{
-       console.log("about to plaid..", req.params.id);
-       if(err){
-         response.json('What are you doing?')
-       } else {
-         plaidClient.getConnectUser(res.access_token, { "pending":true, "gte": "2016-01-11T15:56:46-06:00" }, (err, res2)=>{
-          response.json(res2)
+  .get('/plaidTransactions/:id', (req, response) => {
+    console.log("Hit the endpoint...")
+    User.findById(req.params.id).exec((err, res) => {
+      console.log("about to plaid..", req.params.id);
+      if (err) {
+        response.json('What are you doing?')
+      } else {
+        plaidClient.getConnectUser(res.access_token, {
+            "pending": true,
+            "gte": "2016-01-11T15:56:46-06:00"
+          }, (err, res2) => {
+            response.json(res2)
 
-          //  let flag = false;
-          //  let plaidtrans = res2.transactions.map(function(transaction){
-          //    return transaction._id
-          //  })
-          //  console.log(plaidtrans)
-          //  Transaction.find({plaid_id: {$in: plaidtrans}}).count((err, count) => {
-          //    console.log(err)
-          //       console.log('count', count)
-          //     })
+            //  let flag = false;
+            //  let plaidtrans = res2.transactions.map(function(transaction){
+            //    return transaction._id
+            //  })
+            //  console.log(plaidtrans)
+            //  Transaction.find({plaid_id: {$in: plaidtrans}}).count((err, count) => {
+            //    console.log(err)
+            //       console.log('count', count)
+            //     })
 
 
-          //  for(var i = 0; i < res2.transactions.length; i++){
-          //    let transaction = res2.transactions[i]
-          //    let breaker = false;
-          //    Transaction.find({plaid_id: transaction._id}).exec().then(
-          //      (err, doc) => {
-          //        console.log(doc.length)
-          //        if(doc.length === 0){
-          //          console.log('New Transaction!')
-          //          let newTransaction = new Transaction({
-          //            user: req.params.id,
-          //            account: transaction._account,
-          //            amount: transaction.amount,
-          //            plaid_id: transaction._id,
-          //            posted: transaction.date
-          //          })
-          //          newTransaction.save((err, doc)=>{
-          //            err ? console.log(err) : breaker = false;
-          //          })
-          //        } else if(doc.length > 0) {
-          //          console.log('Flipping the breaker')
-          //          breaker = true;
-          //          console.log('BREAKER', breaker)
-          //        }
-          //        console.log('CONDITIONAL END', '.then END')
-          //    })//end of .then
-          //    console.log("end of for loop")
-          //  }//end of for loop
+            //  for(var i = 0; i < res2.transactions.length; i++){
+            //    let transaction = res2.transactions[i]
+            //    let breaker = false;
+            //    Transaction.find({plaid_id: transaction._id}).exec().then(
+            //      (err, doc) => {
+            //        console.log(doc.length)
+            //        if(doc.length === 0){
+            //          console.log('New Transaction!')
+            //          let newTransaction = new Transaction({
+            //            user: req.params.id,
+            //            account: transaction._account,
+            //            amount: transaction.amount,
+            //            plaid_id: transaction._id,
+            //            posted: transaction.date
+            //          })
+            //          newTransaction.save((err, doc)=>{
+            //            err ? console.log(err) : breaker = false;
+            //          })
+            //        } else if(doc.length > 0) {
+            //          console.log('Flipping the breaker')
+            //          breaker = true;
+            //          console.log('BREAKER', breaker)
+            //        }
+            //        console.log('CONDITIONAL END', '.then END')
+            //    })//end of .then
+            //    console.log("end of for loop")
+            //  }//end of for loop
 
-           })
+          })
           //  response.json(res2.transactions);
-       }
-     })
-   })
-   .post('/user/', (req, response)=>{
+      }
+    })
+  })
+  .post('/user/', (req, response) => {
 
-   })
-   .patch('/user/webhook/:id', (req, response)=>{
-     User
+  })
+  .patch('/user/webhook/:id', (req, response) => {
+    User
       .findById(req.params.id)
-        .exec((err, doc)=>{
-           console.log("about to plaid..", req.params.id);
-           if(err){
-             response.json('What are you doing?')
-           } else {
-             plaidClient.patchConnectUser(doc.access_token, {}, {
-               webhook: 'http://9b7aeccc.ngrok.io/webhook',
-              }, function(err, mfaResponse, respo) {
-               response.json(respo);
-              })
-           }
-         })
-       })
-   .post('/webhook', (req, response)=>{
-     console.log('WEBHOOK ACTIVATED')
-     console.log(req)
-     console.log(Date(Date.now()))
-     req.body.access_token;
-     req.body.total_transactions;
-     response.status(200).json({ title: 'JSON OBJECT', timestamp: Date(Date.now())})
-   })
+      .exec((err, doc) => {
+        console.log("about to plaid..", req.params.id);
+        if (err) {
+          response.json('What are you doing?')
+        } else {
+          plaidClient.patchConnectUser(doc.access_token, {}, {
+            webhook: 'http://9b7aeccc.ngrok.io/webhook',
+          }, function (err, mfaResponse, respo) {
+            response.json(respo);
+          })
+        }
+      })
+  })
+  .post('/webhook', (req, response) => {
+    console.log('WEBHOOK ACTIVATED')
+    console.log(req)
+    console.log(Date(Date.now()))
+    req.body.access_token;
+    req.body.total_transactions;
+    response.status(200).json({
+      title: 'JSON OBJECT',
+      timestamp: Date(Date.now())
+    })
+  })
   //       plaidClient.getAuthUser(access_token, (err, res)=> {
   //         if (err != null) {
   //           console.log(err)
@@ -237,4 +248,4 @@ app
 
 // setTimeout(()=> console.log(plaidClient), 5000)
 
-app.listen(3001, ()=> console.log('Listening on 3001'));
+app.listen(3001, () => console.log('Listening on 3001'));
--- mongoose.js
+++ mongoose.js
@@ -5,8 +5,8 @@ mongoose.Promise = require("q").Promise;
 
 module.exports = exports = {}
 
-exports.db = function(){
-  return mongoose.connect(`mongodb://${secrets.secrets.mongo_user}:${secrets.secrets.mongo_key}@ds039135.mongolab.com:39135/pushbudget`, function(){
-    console.log('connected')
-  })
+exports.db = function () {
+  return mongoose.connect(`mongodb://${secrets.secrets.mongo_user}:${secrets.secrets.mongo_key}@ds039135.mongolab.com:39135/pushbudget`, function () {
+    console.log('connected');
+  });
 }
