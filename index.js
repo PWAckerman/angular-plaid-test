eaf2e58c415879f42867774a29089c6facdee7c7
Brendan Doyle <bdoyle522@gmail.com>
2016-01-14 13:30:52 -0600

    see it this time?

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 let plaid = require('plaid'),
+  session = require('express-session'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy,
+  bCrypt = require('bcrypt-nodejs'),
+  flash = require('connect-flash'),
   express = require('express'),
   plaid_env = plaid.environments.tartan,
   plaid_config = require('./secrets'),
@@ -10,7 +15,7 @@ let plaid = require('plaid'),
   app = express(),
   User = require("./models/user.model.js"),
   Transaction = require("./models/transaction.model.js"),
-  userCtrl = require("./controllers/user.controller.js"),
+  userCtrl = require("./controllers/user.server.controller.js"),
   secrets = require("./secrets.js"),
   db = mongoo.db()
 
@@ -19,6 +24,11 @@ let plaid = require('plaid'),
 db.connection.once('open', () => {
   console.log('Db is connected')
 })
+
+
+
+
+
 
 let plaidTestUser = {}
 if (environment === 'test') {
@@ -32,6 +42,22 @@ if (environment === 'development') {
 app
   .use(bodyParser.json())
   .use(express.static(__dirname))
+  .use(session({secret: 'something'}))
+  .use(passport.initialize())
+  .use(passport.session())
+  .use(flash())
+  .use(function(req, res, next) {
+      res.header('Access-Control-Allow-Credentials', true);
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+      if ('OPTIONS' == req.method) {
+          res.send(200);
+      } else {
+          next();
+      }
+  })
+
   .post('/authenticate', function (req, response) {
     var public_token = req.body.public_token;
     plaidClient.exchangeToken(public_token, (err, res) => {
@@ -64,7 +90,7 @@ app
   })
 
 // break out into user routes/controllers
-.get('/users/all', (req, response) => {
+  .get('/users/all', (req, response) => {
     User.find((err, res) => {
       response.json(res)
     })
@@ -114,10 +140,120 @@ app
       title: 'JSON OBJECT',
       timestamp: Date(Date.now())
     })
-  })
+  });
 
 
 
+
+
+
+  var isValidPassword = function(user, password){
+    return bCrypt.compareSync(password, user.password);
+  }
+  var createHash = function(password){
+   return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+  }
+
+  passport.serializeUser(function(user, done){
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+          done(err, user);
+      });
+  });
+
+  passport.use('signup', new LocalStrategy({passReqToCallback : true},
+    function(req, username, password, done) {
+      console.log('we got here');
+      findOrCreateUser = function(){
+        // find a user in Mongo with provided username
+        User.findOne({'userName':username},function(err, user) {
+          // In case of any error return
+          if (err){
+            console.log('Error in SignUp: '+err);
+            return done(err);
+          }
+          // already exists
+          if (user) {
+            console.log('User already exists');
+            return done(null, false)
+              req.flash('message','User Already Exists');
+          } else {
+            // if there is no user with that email
+            // create the user
+            var newUser = new User();
+            // set the user's local credentials
+            newUser.username = username;
+            newUser.password = createHash(password);
+
+            // save the user
+            newUser.save(function(err) {
+              if (err){
+                console.log('Error in Saving user: '+err);
+                throw err;
+              }
+              console.log('User Registration succesful');
+              return done(null, newUser);
+            });
+          }
+        });
+      };
+      process.nextTick(findOrCreateUser);
+    }
+  ));
+
+  passport.use('login', new LocalStrategy({passReqToCallback : true},
+    function(req, username, password, done) {
+      console.log('we out here');
+      // check in mongo if a user with username exists or not
+      User.findOne({ 'userName' :  username },
+        function(err, user) {
+          // In case of any error, return using the done method
+          if (err){
+            console.log(err);
+            return done(err);
+          }
+          // Username does not exist, log error & redirect back
+          if (!user){
+            console.log('User Not Found with username '+username);
+            return done(null, false,
+                  req.flash('message', 'User Not found.'));
+          }
+          // User exists but wrong password, log the error
+          if (!isValidPassword(user, password)){
+            console.log('Invalid Password');
+            return done(null, false,
+                req.flash('message', 'Invalid Password'));
+          }
+          // User and password both match, return user from
+          // done method which will be treated like success
+          return done(null, user);
+        }
+      );
+  }));
+
+
+  app.get('/', function(req, res) {
+      // Display the Login page with any flash message, if any
+      // res.render('index', { message: req.flash('message') });
+  });
+
+
+  app.post('/login', passport.authenticate('login',
+    {
+      successRedirect: '/#/createLineup',
+      failureRedirect: '/home',
+      failureFlash: true
+  }));
+
+  app.post('/signup', passport.authenticate('signup',
+      {
+        successRedirect: '/#/createLineup',
+        failureRedirect: '/#/login',
+        failureFlash: true
+  }));
 
 
 // setTimeout(()=> console.log(plaidClient), 5000)
